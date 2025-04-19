8371a5042eac73abf79b0c91afe5acd296618b28
kirkrohani <kirk.rohani@gmail.com>
2025-04-18 21:06:10 -0500

    created users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+/**
+ *  User created modules
+ */
+import { UsersModule } from 'src/users/users.module';
+
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
